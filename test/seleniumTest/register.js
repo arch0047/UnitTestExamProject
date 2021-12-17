6e79d321146f722f974c422f7d5d1fbd907bd5e7
Stefanos <sagelastos@gmail.com>
2021-12-17 10:22:56 +0100

    fix aoutput and add pause

--- test/seleniumTest/register.js
+++ test/seleniumTest/register.js
@@ -26,7 +26,7 @@ let test_cases = [
         expect_succes: false
     },
     {
-        description: 'Test case 3: Invalid password should not register',
+        description: 'Test case 4: Invalid password should not register',
         email: 'ANDC@kea.dk',
         activation_code: 'ANDL',
         password: 'WRONG',
@@ -34,7 +34,7 @@ let test_cases = [
         expect_succes: false
     },
     {
-        description: 'Test case 3: Invalid repeat password should not register',
+        description: 'Test case 5: Invalid repeat password should not register',
         email: 'ANDC@kea.dk',
         activation_code: 'ANDL',
         password: '12345678',
@@ -42,6 +42,16 @@ let test_cases = [
         expect_succes: false
     }
 ]
+
+function log_failure(message) {
+    console.error("\x1b[31m", message, '\x1b[0m');
+}
+
+function log_success(message) {
+    console.error("\x1b[32m", message, '\x1b[0m');
+}
+
+const pause_duration = 1000;
 
 test_cases.forEach(async (test_case) => {
 
@@ -51,48 +61,58 @@ test_cases.forEach(async (test_case) => {
             await driver.getTitle().then(function (title) {
                 console.log(title)
                 if (title === 'Register Page') {
-                    console.log('Test 1 passed for reaching the Register Page');
+                    log_success('Test passed for reaching the Register page, ' + test_case.description);
                 } else {
-                    console.log('Test 1 failed for reaching the Register Page');
+                    log_failure('Test failed for reaching the Register page, ' + test_case.description);
                 }
 
             });
         });;
+
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
 
         const email = test_case.email
         const id = driver.findElement(By.id('email'))
         await id.sendKeys(email).then();
 
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
+
         const activation_code = test_case.activation_code
         const activation_code_id = driver.findElement(By.id('activation-code'))
         await activation_code_id.sendKeys(activation_code).then();
 
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
 
         const password = test_case.password
         const password_id = driver.findElement(By.id('password'))
         await password_id.sendKeys(password).then();
 
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
+
         repeat_password = test_case.repeat_password
         const repeat_password_id = driver.findElement(By.id('repeat-password-input'))
         await repeat_password_id.sendKeys(repeat_password).then();
 
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
+
         await driver.findElement(By.id('register-button')).click().then();
+
+        await new Promise(resolve => setTimeout(resolve, pause_duration));
 
         await driver.wait(until.titleIs('Login Page'), 1000).then(async function () {
             await driver.getTitle().then(function (title) {
                 console.log(title)
                 if (title === 'Login Page') {
-                    console.log('Test 2 passed for redirecting to the login page');
+                    console.log();
+                    log_success('Test passed for redirecting to the login page' + test_case.description);
                 } else {
-                    console.log('Test 2 failed for redirecting to the login page');
+                    log_failure('Test failed for redirecting to the login page' + test_case.description);
                 }
             });
         }).catch(err => {
-            if (test_case.expect_succes) {
-                console.log("---------------------Test failed!", test_case.description);
-            }
-            console.log("-------------Test passed!", test_case.description);
-        });;
+                if (test_case.expect_succes) log_failure("Test failed for Teacher page, " + test_case.description);
+                else log_success("Test passed for Teacher page, " + test_case.description);
+            });
     }
 
     finally {
